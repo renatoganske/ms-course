153fefbf60cb38c347674b7bcf1380f8b3e6952b
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-02-28 10:35:46 -0300

    Feign Client

--- hr-payroll/src/main/java/io/github/renatoganske/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/io/github/renatoganske/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package io.github.renatoganske.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
