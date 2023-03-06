fb77200de88c2157fc5f3998c8de8a622adb0ff4
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-03-06 16:47:02 -0300

    Database seeding

--- hr-user/src/main/java/io/github/renatoganske/hruser/HrUserApplication.java
+++ hr-user/src/main/java/io/github/renatoganske/hruser/HrUserApplication.java
@@ -1,15 +1,27 @@
 package io.github.renatoganske.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner{
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		método para fazer o hash da senha do usuário seed
+//		System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
 	}
 
 }
